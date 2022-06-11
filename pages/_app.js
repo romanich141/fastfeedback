b33aaec01a3d7d29b5ee57f5d00f7deb379c08a7
Roma Ovcharenko <roma_ovcharenko@ukr.net>
2022-06-11 16:38:53 +0300

    add chakre ui; add absolutly imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,42 @@
-import { AuthProvider } from '../lib/auth'
-import '../styles/globals.css'
+import { CSSReset, ChakraProvider } from '@chakra-ui/react';
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme';
+import Head from 'next/head';
+import { Global, css } from '@emotion/react';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <CSSReset />
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   )
 }
 
---
+++ styles/theme.js
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const fonts = {
+  body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+};
+
+const fontWeights = {
+  normal: 400,
+  medium: 600,
+  bold: 800,
+};
+
+const theme = extendTheme({ fonts, fontWeights })
+
+export default theme;
